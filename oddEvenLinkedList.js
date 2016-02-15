60e49e117a9e9adecd6ae9b16a1ffe65cac3f2b5
cauil <xiaoloujun@gmail.com>
2016-02-15 10:45:25 +0800

    add linked list problems solution

---
+++ oddEvenLinkedList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(!head) return null;
+    var odd = head;
+    var even = head.next;
+    var evenHead = even;
+    while(even && even.next) {
+        odd.next = odd.next.next;
+        even.next = even.next.next;
+        odd = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+};
